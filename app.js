0644023c2ccc32bb009126a678fba0e10ed52e44
javeria-farooq <gettingeducation6@gmail.com>
2024-01-24 22:13:33 +0500

    23 JS Assignment

---
+++ app.js
@@ -0,0 +1,43 @@
+// let firstObj = {
+//     Email: 'javeria@',
+//     Password: '1234'
+// }
+
+// let secObj = {
+//     Email: 'aliza@',
+//     Password: '5678'
+// }
+
+// let thirdObj = {
+//     Email: 'afnan@',
+//     Password: '910'
+// }
+
+// let arr = [firstObj, secObj, thirdObj]
+// let sto = JSON.stringify(arr)
+// localStorage.setItem('Email', sto)
+
+// let getting = localStorage.getItem('Email')
+
+// let lEmail = document.getElementById('lEmail').value
+// let lPass = document.getElementById('lPass').value
+
+// function login() {
+//     for(var i = 0; i <= getting; i++) {
+//         document.write(getting[i])
+//     }
+//     if (lEmail === getting[i]) {
+//         alert('ok')
+//     }
+// }
+
+// function login() {
+//     let lEmail = document.getElementById('lEmail').value
+//     let lPass = document.getElementById('lPass').value
+    // localStorage.setItem('Email', lEmail)
+    // localStorage.setItem('Password', lPass)
+// }
+
+// function login() {
+//     if ()
+// }
